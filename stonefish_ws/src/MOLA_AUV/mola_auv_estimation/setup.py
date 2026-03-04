edf24db92342b458d9f396b75250a91d507b7be8
AlePuglisi <puglisialessandro27@gmail.com>
2026-03-04 01:21:43 +0100

    feat(mola): adding plotting node

--- stonefish_ws/src/MOLA_AUV/mola_auv_estimation/setup.py
+++ stonefish_ws/src/MOLA_AUV/mola_auv_estimation/setup.py
@@ -25,6 +25,8 @@ setup(
     entry_points={
         'console_scripts': [
             'mola_auv_apriltag = mola_auv_estimation.mola_auv_apriltag:main',
+            # 'mola_auv_ekf = mola_auv_estimation.mola_auv_ekf:main',
+            'mola_auv_plot = mola_auv_estimation.mola_auv_plot:main',           
         ],
     },
 )
